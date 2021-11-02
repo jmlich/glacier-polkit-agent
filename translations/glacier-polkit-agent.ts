db3ae3654a8ed88047b21ee498ccd386f4305522
Jozef Mlich <jmlich83@gmail.com>
2021-11-02 16:46:06 +0000

    lupdate

---
+++ translations/glacier-polkit-agent.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../main.cpp" line="60"/>
+        <source>Authentication</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>glacier-polkit-agent</name>
+    <message>
+        <location filename="../qml/glacier-polkit-agent.qml" line="46"/>
+        <source>Authentication</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/glacier-polkit-agent.qml" line="70"/>
+        <source>Enter you password:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/glacier-polkit-agent.qml" line="84"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/glacier-polkit-agent.qml" line="99"/>
+        <source>Accept</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
